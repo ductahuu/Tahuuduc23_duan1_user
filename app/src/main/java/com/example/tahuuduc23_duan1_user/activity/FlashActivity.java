8046df6d69ecc1d15a67b4d7a4843c1b29f91df8
admin <92538296+ductahuu@users.noreply.github.com>
2023-04-04 15:25:50 +0700

    Screen LoginActivity

---
+++ app/src/main/java/com/example/tahuuduc23_duan1_user/activity/FlashActivity.java
@@ -0,0 +1,17 @@
+package com.example.tahuuduc23_duan1_user.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tahuuduc23_duan1_user.R;
+import com.example.tahuuduc23_duan1_user.R;
+
+public class FlashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash);
+    }
+}
---
+++ app/src/main/java/com/example/tahuuduc23_duan1_user/activity/LoginActivity.java
@@ -0,0 +1,6 @@
+package com.example.tahuuduc23_duan1_user.activity;
+
+import android.app.Activity;
+
+public class LoginActivity extends Activity {
+}
